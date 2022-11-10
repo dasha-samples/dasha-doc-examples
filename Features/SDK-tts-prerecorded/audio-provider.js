3525acb2d977ca835a86ca35e95d7c8e25a3267f
Vladimir <vkujantsev94@gmail.com>
2022-11-10 19:35:16 +0700

    fixes

--- Features/SDK-tts-prerecorded/audio-provider.js
+++ Features/SDK-tts-prerecorded/audio-provider.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const dasha = require("@dasha.ai/sdk");
 
-
 const defaultVoiceInfo = {
   lang: undefined,
   speaker: "default",
@@ -29,18 +28,30 @@ class AudioProvider {
 
   /** Apply audioProvider to Dasha application */
   applyToApp(app) {
-    app.customTtsProvider = async (text, voice) => {
-      console.log(`Got resource request for ${JSON.stringify({text, voice})}`);
-      /** get audio file path for provided text and voice */
-      const fname = this.getAudioResourcePath(text, voice);  
-      console.log(`Found audio resource for "${text}" in file ${fname}`);
-      /** read file and send it as response */
-      return dasha.audio.fromFile(fname);
-    };
+    /** set AudioProvider.handleTtsRequest method as tts provider for dasha application */
+    app.customTtsProvider = this.handleTtsRequest;
   } 
 
   /**
+   * Handles tts request from dasha runtime application
+   * @param {string} text phrase text
+   * @param {object} voiceInfo phrase voice information: lang, speaker, emotion, speed, variation
+   * @returns dasha.audio.Audio object of a resource file.
+   */
+  async handleTtsRequest(text, voiceInfo) {
+    console.log(`Got resource request for ${JSON.stringify({text, voiceInfo})}`);
+    /** get audio file path for provided text and voiceInfo */
+    const fname = this.getAudioResourcePath(text, voiceInfo);  
+    console.log(`Found audio resource for "${text}" in file ${fname}`);
+    /** read file and send it as response */
+    return dasha.audio.fromFile(fname);
+  };
+
+  /**
    * Build key for phrase audio resource
+   * @param {string} text 
+   * @param {object} voiceInfo 
+   * @returns string that is unique for provided text and voiceInfo
    */
   getResourceKey(text, voiceInfo) {
     voiceInfo = { ...defaultVoiceInfo, ...voiceInfo };
@@ -59,6 +70,7 @@ class AudioProvider {
    * @param {string[]|object[]} audioConfigOrPath audio resource config or path to file with config
    */  
   loadConfig(audioConfigOrPath) {
+    /** if provided argument is a string, it is considered as path to config */
     let audioConfig = undefined;
     let isPath = false;
     if (typeof audioConfigOrPath === "string") {
@@ -67,47 +79,38 @@ class AudioProvider {
     } else {
       audioConfig = audioConfigOrPath;
     }
-    const resourcePath = audioConfig.resource_path;
-    if (resourcePath === undefined) {
-      throw Error(`Audio config does not have property 'resource_path'`);
-    }
+
     const errors = [];
     const pharses = audioConfig.phrases ?? [];
     let i = 0;
+    /** walk through the phrase descriptions in config, validate them and save if they are ok */
     for (const phrase of pharses) {
       i++;
       /** validate phrase description */
       if (phrase.phrase === undefined) {
-        errors.push(
-          `Phrase ${i}/${pharses.length} does not have property 'phrase'`
-        );
+        errors.push(`Phrase ${i}/${pharses.length} does not have property 'phrase'`);
         continue;
       }
       const phraseShort = phrase.phrase.length < 20 ? phrase.phrase : `${phrase.phrase.slice(0, 10)}...`;
       if (phrase.audio === undefined) {
-        errors.push(
-          `Phrase ${i}/${pharses.length} "${phraseShort}" does not have property 'audio'`
-        );
+        errors.push(`Phrase ${i}/${pharses.length} "${phraseShort}" does not have property 'audio'`);
         continue;
       }
       /** check audio resource exists */
       let audioFilePath = phrase.audio;
       if (isPath) {
-        path.
+        /** if config was provided as a path, resolve path to audio file */
         audioFilePath = path.join(path.dirname(path.resolve(audioConfigOrPath)), audioFilePath);
       }
-      
       if (!fs.existsSync(audioFilePath)) {
-        errors.push(
-          `Phrase ${i}/${pharses.length} "${phraseShort}" file not found in path '${audioFilePath}'`
-        );
+        errors.push(`Phrase ${i}/${pharses.length} "${phraseShort}" file not found in path '${audioFilePath}'`);
         continue;
       }
       const voiceInfo = { ...audioConfig.voice, ...phrase.voice };
       /** build phrase key */
       const resourceKey = this.getResourceKey(phrase.phrase, voiceInfo);
       if (resourceKey in this.resources) {
-        /** duplicate is not an error */
+        /** duplicate is not considered as an error */
         console.warn(
           `Skipping ${i}/${pharses.length} phrase "${phraseShort}" because it duplicates previous phrase`
         );
@@ -115,6 +118,7 @@ class AudioProvider {
       }
       this.resources[resourceKey] = audioFilePath;
     }
+    /** if there were any invalid phrases, throw an error all the issues */
     if (errors.length > 0) {
       throw new Error(
         `Could not load audio resources config. Errors: [${errors
@@ -135,13 +139,19 @@ class AudioProvider {
     }
   }
 
+  /**
+   * Get saved audio resource path for provided text and voice info.
+   */
   getAudioResourcePath(text, voiceInfo) {
+    /** build key */
     const key = this.getResourceKey(text, voiceInfo);
     const resourcePath = this.resources[key];
+    /** check if key is valid */
     if (resourcePath === undefined)
       throw new Error(`Failed to get resource path for key '${key}'`);
     return resourcePath;
   }
+  /** Get path to audio resource, read and return audio */
   getAudioResource(text, voiceInfo) {
     const resourcePath = this.getAudioResourcePath(text, voiceInfo);
     return fs.readFileSync(resourcePath);
--- Features/SDK-tts-prerecorded/index.js
+++ Features/SDK-tts-prerecorded/index.js
@@ -1,17 +1,5 @@
 const dasha = require("@dasha.ai/sdk");
-const AudioProvider = require("./AudioProvider");
-
-/** Apply audioProvider to Dasha application */
-function applyCustomeTtsToApp(audioProvider, app) {
-  app.customTtsProvider = async (text, voice) => {
-    console.log(`Got resource request for ${JSON.stringify({ text, voice })}`);
-    /** get audio file path for provided text and voice */
-    const fname = audioProvider.getAudioResourcePath(text, voice);
-    console.log(`Found audio resource for "${text}" in file ${fname}`);
-    /** read file and send it as response */
-    return dasha.audio.fromFile(fname);
-  };
-}
+const AudioProvider = require("./audio-provider");
 
 async function main() {
   if (process.argv[2] === undefined)
@@ -21,21 +9,22 @@ async function main() {
   /** prepare audio resource */
   const audioProvider = AudioProvider.create(["audio-resources-config.json"]);
   console.log("Successfully loaded audio resource config")
-  console.log(audioProvider.resources)
 
-  // deploy and start application located at .dashaapp file path
+  /** deploy and start application located at .dashaapp file path */
   const app = await dasha.deploy("./app");
-
-  applyCustomeTtsToApp(audioProvider, app);
-
+  /** use audioProvider as custom tts handler */
+  audioProvider.applyToApp(app);
   await app.start({ concurrency: 1 });
-  // create conversation with provided endpoint
+
+  /** create conversation with provided endpoint */
   const conv = app.createConversation({
     endpoint: endpoint,
   });
+  /** use custom tts handler for this conversation */
   conv.audio.tts = "custom";
   conv.on("transcription", console.log);
-  // execute conversation with corresponding channel
+
+  /** execute conversation with corresponding channel */
   const result = await conv.execute({ channel: "audio" });
   console.log("conversation result", result.output);
 
